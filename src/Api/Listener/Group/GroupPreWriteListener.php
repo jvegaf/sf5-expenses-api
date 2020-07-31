ad0d3274837fb8d47ae13ec7035aa0228ea3832e
Jose Vega <josevega234@gmail.com>
2020-07-31 20:50:50 +0200

    mas a medias

--- src/Api/Listener/Group/GroupPreWriteListener.php
+++ src/Api/Listener/Group/GroupPreWriteListener.php
@@ -6,12 +6,16 @@ namespace App\Api\Listener\Group;
 
 
 use App\Api\Listener\PreWriteListener;
+use App\Entity\Group;
+use App\Entity\User;
+use App\Exception\Group\CanNotAddAnotherOwnerException;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
-use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class GroupPreWriteListener implements PreWriteListener
 {
     private TokenStorageInterface $tokenStorage;
+    private const POST_GROUP = 'api_groups_post_collection';
 
     public function __construct(TokenStorageInterface $tokenStorage)
     {
@@ -20,6 +24,19 @@ class GroupPreWriteListener implements PreWriteListener
 
     public function onKernelView(ViewEvent $event): void
     {
-        // TODO: Implement onKernelView() method.
+        /** @var User $tokenUser */
+        $tokenUser = $this->tokenStorage->getToken()->getUser();
+        $request = $event->getRequest();
+
+        if (self::POST_GROUP === $request->get('_route')){
+            /** @var Group $group */
+            $group = $event->getControllerResult();
+
+            if (!$group->isOwnedBy($tokenUser)){
+                throw CanNotAddAnotherOwnerException::create();
+            }
+
+            $group->addUser($tokenUser);
+        }
     }
 }
---
+++ src/Exception/Group/CanNotAddAnotherOwnerException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Exception\Group;
+
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class CanNotAddAnotherOwnerException extends AccessDeniedHttpException
+{
+    private const MESSAGE = 'You can not add another user as owner';
+
+    public static function create(): self
+    {
+        throw new self(self::MESSAGE);
+    }
+}
